d9b1a4766b0e4bc16769a8e36f24838348935014
Claudio Carnino <claudio@tugulab.org>
2023-11-27 18:37:10 +0200

    done threads and references ones

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -31,10 +29,14 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
+    let tx_arc = Arc::new(tx);
+    let tx_arc1 = Arc::clone(&tx_arc);
+    let tx_arc2 = Arc::clone(&tx_arc);
+
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx_arc1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -42,7 +44,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx_arc2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
